7faff0f8c118efc7038275da47bba1cb745473ea
Krishna <Krishna@Krishna>
2023-01-28 18:26:34 +0530

    [krishna]: update happen

--- src/Sample1/Demo2.java
+++ src/Sample1/Demo2.java
@@ -1,5 +1,11 @@
 package Sample1;
 
-public class Demo2 {
+public class Demo2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hello krishna..! Its nice to meet you..");
+		
+	}
 
 }
